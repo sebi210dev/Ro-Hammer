20ed753d2b8dd66c7dab13fbe10d92c1d7a0042b
sebi210dev <59232422+sebi210dev@users.noreply.github.com>
2020-03-09 16:59:38 +0100

    Fixed Webhook not sending

--- src/ServerScriptService/Hammer/Webhooks.lua
+++ src/ServerScriptService/Hammer/Webhooks.lua
@@ -3,7 +3,7 @@ local HttpService = game:GetService("HttpService")
 
 function Webhooks:SendGotBanned(URL, Player, PersonWhoBanned)
     local Date = os.date("!*t")
-    local Content = {
+    --[[local Content = {
         embeds = {
             {
                 title = "Hammer - Ban successful";
@@ -20,9 +20,30 @@ function Webhooks:SendGotBanned(URL, Player, PersonWhoBanned)
                 }
             }
         }
+    }--]]
+
+    --// This is just my testing –– sebi210
+
+    local Data = {
+        ["embeds"] = {
+            {
+                ["title"] = "Hammer - Ban successful";
+                ["color"] = Color3.fromRGB(255, 0, 0);
+                ["fields"] = {
+                    {
+                        ["name"] = "Player banned:";
+                        ["value"] = Player.Name
+                    };
+                    {
+                        ["name"] = "Banned at:";
+                        ["value"] = string.format("%d:%d:%d %d/%d/%d", Date.hour, Date.min, Date.sec, Date.day, Date.month, Date.year)
+                    };
+                }
+            }
+        }  
     }
 
-    local Encoded = HttpService:JSONEncode(Content)
+    local Encoded = HttpService:JSONEncode(Data)
     print(Encoded)
     HttpService:PostAsync(URL, Encoded)
 end
