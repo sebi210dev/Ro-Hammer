8fa683a6115b94a8f51638db0c6b5da09c5f9470
sebi210dev <59232422+sebi210dev@users.noreply.github.com>
2020-03-09 17:01:17 +0100

    Finished Webhooks

--- src/ServerScriptService/Hammer/Webhooks.lua
+++ src/ServerScriptService/Hammer/Webhooks.lua
@@ -3,27 +3,6 @@ local HttpService = game:GetService("HttpService")
 
 function Webhooks:SendGotBanned(URL, Player, PersonWhoBanned)
     local Date = os.date("!*t")
-    --[[local Content = {
-        embeds = {
-            {
-                title = "Hammer - Ban successful";
-                color = Color3.fromRGB(255, 0, 0);
-                fields = {
-                    {
-                        name = "Player banned:";
-                        value = Player
-                    };
-                    {
-                        name = "Banned at:";
-                        value = string.format("%d:%d:%d %d/%d/%d", Date.sec, Date.min, Date.hour, Date.day, Date.month, Date.year)
-                    };
-                }
-            }
-        }
-    }--]]
-
-    --// This is just my testing –– sebi210
-
     local Data = {
         ["embeds"] = {
             {
